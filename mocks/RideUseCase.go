be7b2bcfb5cb3093c7ae6c975087dc71374d84c8
Rafael Piovesan <rubens.piovesan@gmail.com>
2021-12-23 10:47:59 -0300

    chore(mocks): refreshing ride uc mock

--- mocks/RideUseCase.go
+++ mocks/RideUseCase.go
@@ -15,15 +15,24 @@ type RideUseCase struct {
 }
 
 // Create provides a mock function with given fields: ctx, ik, rd
-func (_m *RideUseCase) Create(ctx context.Context, ik *entity.IdempotencyKey, rd *entity.Ride) error {
+func (_m *RideUseCase) Create(ctx context.Context, ik *entity.IdempotencyKey, rd *entity.Ride) (*entity.IdempotencyKey, error) {
 	ret := _m.Called(ctx, ik, rd)
 
-	var r0 error
-	if rf, ok := ret.Get(0).(func(context.Context, *entity.IdempotencyKey, *entity.Ride) error); ok {
+	var r0 *entity.IdempotencyKey
+	if rf, ok := ret.Get(0).(func(context.Context, *entity.IdempotencyKey, *entity.Ride) *entity.IdempotencyKey); ok {
 		r0 = rf(ctx, ik, rd)
 	} else {
-		r0 = ret.Error(0)
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(*entity.IdempotencyKey)
+		}
 	}
 
-	return r0
+	var r1 error
+	if rf, ok := ret.Get(1).(func(context.Context, *entity.IdempotencyKey, *entity.Ride) error); ok {
+		r1 = rf(ctx, ik, rd)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
 }
