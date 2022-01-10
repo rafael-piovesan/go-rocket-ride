9d0f9785c25796a6ba61640a9229209c21d165a1
Rafael Piovesan <rubens.piovesan@gmail.com>
2022-01-10 16:42:36 -0300

    feat(config): validating app cfg

--- config.go
+++ config.go
@@ -1,32 +1,59 @@
 package rocketride
 
 import (
+	"fmt"
+	"log"
+	"path"
+
+	"github.com/go-playground/validator/v10"
 	"github.com/spf13/viper"
+)
+
+const (
+	cfgFileName = "app"
+	cfgFileType = "env"
 )
 
 // Config stores all configuration of the application.
 // The values are read by viper from a config file or environment variable.
 type Config struct {
-	IdemKeyTimeout int    `mapstructure:"IDEM_KEY_TIMEOUT"`
-	DBSource       string `mapstructure:"DB_SOURCE"`
-	ServerAddress  string `mapstructure:"SERVER_ADDRESS"`
-	StripeKey      string `mapstructure:"STRIPE_KEY"`
+	IdemKeyTimeout int    `mapstructure:"IDEM_KEY_TIMEOUT" validate:"required"`
+	DBSource       string `mapstructure:"DB_SOURCE"  validate:"required"`
+	ServerAddress  string `mapstructure:"SERVER_ADDRESS"  validate:"required"`
+	StripeKey      string `mapstructure:"STRIPE_KEY"  validate:"required"`
 }
 
 // LoadConfig reads configuration from file or environment variables.
-func LoadConfig(path string) (config Config, err error) {
-	viper.AddConfigPath(path)
-	viper.SetConfigName("app")
-	viper.SetConfigType("env")
-	viper.SetDefault("IdemKeyTimeout", 5)
+func LoadConfig(cfgPath string) (config Config, err error) {
+	// set config file path, name and extension (e.g,'/path-to-config/app.env')
+	viper.AddConfigPath(cfgPath)
+	viper.SetConfigName(cfgFileName)
+	viper.SetConfigType(cfgFileType)
 
+	// bind env vars, to be used in case the config file is not found
+	_ = viper.BindEnv("IDEM_KEY_TIMEOUT")
+	_ = viper.BindEnv("DB_SOURCE")
+	_ = viper.BindEnv("SERVER_ADDRESS")
+	_ = viper.BindEnv("STRIPE_KEY")
+
+	// default config values
+	viper.SetDefault("IDEM_KEY_TIMEOUT", 5)
+
+	// enable loading of env vars values
+	// it'll override the values in the config file
 	viper.AutomaticEnv()
 
 	err = viper.ReadInConfig()
 	if err != nil {
-		return
+		log.Printf("config file '%s' not found", path.Join(cfgPath, fmt.Sprintf("%s.%s", cfgFileName, cfgFileType)))
 	}
 
 	err = viper.Unmarshal(&config)
-	return
+	if err != nil {
+		return
+	}
+
+	v := validator.New()
+	err = v.Struct(&config)
+	return config, err
 }
