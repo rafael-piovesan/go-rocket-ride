c19c61e0ab4e39a5b2531d0117965d54dda04aaf
Rafael Piovesan <rubens.piovesan@gmail.com>
2022-01-10 16:43:35 -0300

    feat(tests): add main.go tests

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -10,19 +10,23 @@ import (
 	"github.com/rafael-piovesan/go-rocket-ride/pkg/stripemock"
 	"github.com/uptrace/bun"
 	"github.com/uptrace/bun/dialect/pgdialect"
-	"github.com/uptrace/bun/driver/pgdriver"
+	_ "github.com/uptrace/bun/driver/pgdriver"
 )
 
 func main() {
 	// app's config values
 	cfg, err := rocketride.LoadConfig(".")
 	if err != nil {
-		log.Fatal("cannot load config:", err)
+		log.Fatalf("cannot load config: %v", err)
 	}
 
 	// database connection
 	dsn := cfg.DBSource
-	sqldb := sql.OpenDB(pgdriver.NewConnector(pgdriver.WithDSN(dsn)))
+	sqldb, err := sql.Open("pg", dsn)
+	if err != nil {
+		log.Fatalf("cannot open database: %v", err)
+	}
+
 	db := bun.NewDB(sqldb, pgdialect.New())
 	store := datastore.NewStore(db)
 
---
+++ cmd/api/main_test.go
@@ -0,0 +1,91 @@
+//go:build integration
+// +build integration
+
+package main
+
+import (
+	"bytes"
+	"os"
+	"os/exec"
+	"syscall"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMain(t *testing.T) {
+	if os.Getenv("START_MAIN") == "1" {
+		main()
+		return
+	}
+
+	t.Run("Missing env vars", func(t *testing.T) {
+		stdout, stderr, err := startSubprocess(t, false)
+		if e, ok := err.(*exec.ExitError); ok && !e.Success() {
+			assert.Empty(t, stdout)
+			assert.Contains(t, stderr, "cannot load config")
+			return
+		}
+		t.Fatalf("process ran with err %v, want exit status 1", err)
+	})
+
+	t.Run("Invalid Postgres URL", func(t *testing.T) {
+		stdout, stderr, err := startSubprocess(t, false, "DB_SOURCE=rides", "SERVER_ADDRESS=foo", "STRIPE_KEY=bar")
+		if e, ok := err.(*exec.ExitError); ok && !e.Success() {
+			assert.Empty(t, stdout)
+			assert.Contains(t, stderr, "cannot open database")
+			return
+		}
+		t.Fatalf("process ran with err %v, want exit status 1", err)
+	})
+
+	t.Run("Valid app config values", func(t *testing.T) {
+		_, _, err := startSubprocess(
+			t,
+			true, // force subprocess stop, otherwise it'd keep running
+			"DB_SOURCE=postgresql://usr:pass@localhost:5432/db?sslmode=disable",
+			"SERVER_ADDRESS=0.0.0.0:8000",
+			"STRIPE_KEY=bar",
+		)
+		assert.Nil(t, err)
+	})
+}
+
+// startSubprocess calls "go test" command specifying the test target name "TestMain" and setting
+// the env var "START_MAIN=1". It will cause the test to be run again, but this time calling the
+// "main()" func. This way, it's possible to retrieve and inspect the app exit code along with
+// the stdout and stderr as well.
+// See more at: https://stackoverflow.com/a/33404435
+func startSubprocess(t *testing.T, forceStop bool, envs ...string) (stdout string, stderr string, err error) {
+	var cout, cerr bytes.Buffer
+
+	// call test suit again specifying "TestMain" as the target
+	cmd := exec.Command(os.Args[0], "-test.run=TestMain")
+
+	// set "START_MAIN" env var along with any additional value provided as parameter
+	envs = append(envs, "START_MAIN=1")
+	cmd.Env = append(os.Environ(), envs...)
+
+	// capture subprocess' stdout and stderr
+	cmd.Stdout = &cout
+	cmd.Stderr = &cerr
+
+	err = cmd.Start()
+	if err == nil {
+		go func() {
+			// when instructed to do so, signal subprocess to stop it
+			if forceStop {
+				time.Sleep(time.Second * 1)
+				cmd.Process.Signal(syscall.SIGINT)
+			}
+		}()
+
+		// wait for subprocess completion
+		err = cmd.Wait()
+	}
+
+	stdout = cout.String()
+	stderr = cerr.String()
+	return
+}
